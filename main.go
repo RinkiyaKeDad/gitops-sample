7351114815ba4502ce040f2492903212bbcad58d
Arsh Sharma <arshsharma461@gmail.com>
2023-10-13 15:27:52 +0530

    added job to edit helm value
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	// Write the response to the client
-	fmt.Fprint(w, "Hello, DockerHub!")
+	fmt.Fprint(w, "Hello, GitOps!")
 }
 
 func main() {
